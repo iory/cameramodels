dbc7ba58e0719289b2bcdd5494b7ca0cc6317d91
iory <ab.ioryz@gmail.com>
2019-06-12 00:24:09 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup_requires = []
+install_requires = []
+
+setup(
+    name='cameramodels',
+    version='0.0.1',
+    description='A python library',
+    author='iory',
+    author_email='ab.ioryz@gmail.com',
+    url='https://github.com/iory/cameramodels',
+    license='MIT License',
+    packages=find_packages(),
+    zip_safe=False,
+    setup_requires=setup_requires,
+)
