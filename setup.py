352cd0d22e44693f1adfd53544c75046f9e7a582
iory <ab.ioryz@gmail.com>
2020-01-27 01:54:43 +0900

    For release

--- setup.py
+++ setup.py
@@ -1,7 +1,36 @@
-#!/usr/bin/env python
+from __future__ import print_function
+
+import distutils.spawn
+import shlex
+import subprocess
+import sys
 
 from setuptools import find_packages
 from setuptools import setup
+
+
+version = '0.0.1'
+
+
+if sys.argv[-1] == 'release':
+    if not distutils.spawn.find_executable('twine'):
+        print(
+            'Please install twine:\n\n\tpip install twine\n',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    commands = [
+        'git tag v{:s}'.format(version),
+        'git push origin master --tag',
+        'python setup.py sdist',
+        'twine upload dist/cameramodels-{:s}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        print('+ {}'.format(cmd))
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
 
 setup_requires = []
 install_requires = [
@@ -11,13 +40,28 @@ install_requires = [
 
 setup(
     name='cameramodels',
-    version='0.0.1',
-    description='A python library',
+    version=version,
+    description='A python library for cameramodels',
     author='iory',
     author_email='ab.ioryz@gmail.com',
     url='https://github.com/iory/cameramodels',
-    license='MIT License',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
     packages=find_packages(),
     zip_safe=False,
     setup_requires=setup_requires,
+    install_requires=install_requires,
 )
