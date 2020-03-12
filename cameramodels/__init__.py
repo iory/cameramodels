2045e768399bead44ee3418c87fa514748e6a7dc
iory <ab.ioryz@gmail.com>
2020-03-12 21:36:10 +0900

    Add kinect v2 cameramodel

--- cameramodels/__init__.py
+++ cameramodels/__init__.py
@@ -6,5 +6,6 @@ __version__ = pkg_resources.get_distribution('cameramodels').version
 
 from cameramodels.d415 import D415  # NOQA
 from cameramodels.d435 import D435  # NOQA
+from cameramodels.kinect_v2 import KinectV2  # NOQA
 from cameramodels.pinhole_camera import PinholeCameraModel  # NOQA
 from cameramodels.xtion import Xtion  # NOQA
---
+++ cameramodels/kinect_v2.py
@@ -0,0 +1,44 @@
+from __future__ import division
+
+from cameramodels import pinhole_camera
+
+
+class KinectV2(pinhole_camera.PinholeCameraModel):
+
+    """Camera model class for Kinect v2
+
+    """
+
+    models = {'rgb':
+              {'image_height': 1920,
+               'image_width': 1080,
+               'fov': 84.1},
+              'depth':
+              {'image_height': 512,
+               'image_width': 424,
+               'fov': 70,
+               'far': 4.5,
+               'near': 0.5}}
+
+    def __init__(self, mode='rgb'):
+        if mode not in self.models:
+            raise ValueError
+        height = self.models[mode]['image_height']
+        width = self.models[mode]['image_width']
+        fovy = self.models[mode]['fov']
+        aspect = width / height
+
+        fovx = aspect * fovy
+        fx = self.calc_f_from_fov(fovx, width)
+        fy = self.calc_f_from_fov(fovy, height)
+        K = [fx, 0, width / 2.0,
+             0, fy, height / 2.0,
+             0, 0, 1]
+        P = [fx, 0, width / 2.0, 0,
+             0, fy, height / 2.0, 0,
+             0, 0, 1, 0]
+        super(KinectV2, self).__init__(
+            image_height=height,
+            image_width=width,
+            K=K,
+            P=P)
